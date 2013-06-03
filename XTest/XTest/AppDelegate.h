c4212f069e5aaaaf0595761e16e213f1c91b6d81
lakkey <lakkey@lakkeytekiMac.local>
2013-06-03 22:10:40 +0800

    0.0.1

---
+++ XTest/XTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XTest
+//
+//  Created by lakkey on 13-6-3.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XTest/XTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XTest
+//
+//  Created by lakkey on 13-6-3.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
